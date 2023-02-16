90cd65c2f33280c448e949a40345d2b6d82f0ab8
acui51 <acui@stanford.edu>
2023-02-15 18:33:23 -0800

    move to .env

--- supabase.ts
+++ supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // TODO: move to browser public env vars
-const supabaseUrl = "https://nvpoxnhyhnoxikpasiwv.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52cG94bmh5aG5veGlrcGFzaXd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ3ODAyODgsImV4cCI6MTk5MDM1NjI4OH0.-aCVYj85FKsW48DPlHsG8X2Xk3HTHnJsl2t3o0fg55c";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
