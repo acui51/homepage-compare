85f19c45fbbc8571a880571d2aeaf9f1561f4c28
Matthew Turk <mjturk15@gmail.com>
2023-02-20 15:00:46 -0800

    Renaming

--- src/components/NewsPreview.tsx
+++ src/components/NewsPreview.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Group } from '@visx/group';
 import { Point } from '@visx/point';
-import { scaleLinear } from '@visx/scale';
 import { Line, LineRadial } from '@visx/shape';
 
 // Check out documentation: https://airbnb.io/visx/radar
-const orange = '#ff9933';
-export const pumpkin = '#f5810c';
+const peterRiver = '#3498db';
+const belizeHole = '#2980b9';
 const silver = '#d9d9d9';
-export const background = '#FAF7E9';
-
-const degrees = 360;
+const background = '#FAF7E9';
 
 const y = (d: RadarProminence) => d.prominence;
 
-const genAngles = (length: number) =>
-  [...new Array(length + 1)].map((_, i) => ({
-    angle: i * (degrees / length) + (length % 2 === 0 ? 0 : degrees / length / 2),
-  }));
+function genAngles(length: number) {
+  const interval = [...new Array(length + 1)]
+  return interval.map((_, i) => ({
+    angle: i * (360 / length) + (length % 2 === 0 ? 0 : 360 / length / 2),
+  }))
+}
 
 const genPoints = (length: number, radius: number) => {
   const step = (Math.PI * 2) / length;
@@ -65,65 +64,57 @@ type NewsPreviewProps = {
      * data for rendering polygon in mini Radar
     **/
     radarData: RadarProminence[];
+    
+    /**
+     * margin for mini Radar
+     * default: { top: 40, left: 80, right: 80, bottom: 80 }
+     * */
+    margin?: { top: number; right: number; bottom: number; left: number };
 }
 
-export default function NewsPreview({ radarData }: NewsPreviewProps) {
-    const levels = 5
-    const width = 400
-    const height = 400
-    const margin = defaultMargin
-    const xMax = width - margin.left - margin.right;
+export default function NewsPreview({ radarData, margin = defaultMargin }: NewsPreviewProps) {
+  const nLevels = 5
+  const width = 400
+  const height = 400
+  const xMax = width - margin.left - margin.right;
   const yMax = height - margin.top - margin.bottom;
   const radius = Math.min(xMax, yMax) / 2;
 
-  const radialScale = scaleLinear<number>({
-    range: [0, Math.PI * 2],
-    domain: [degrees, 0],
-  });
+  const radialScale = (d: number) => d * (Math.PI * 2) / 360;
+  const verticalScale = (d: number) => d * radius / Math.max(...radarData.map(y));
 
-  const yScale = scaleLinear<number>({
-    range: [0, radius],
-    domain: [0, Math.max(...radarData.map(y))]
-  });
-
-  const webs = genAngles(radarData?.length);
-  const points = genPoints(radarData?.length, radius);
-  const polygonPoints = genPolygonPoints(radarData, (d) => yScale(d) ?? 0, y);
-  const zeroPoint = new Point({ x: 0, y: 0 });
-    return radarData ? (
-        <div>
-            <h1>{JSON.stringify(radarData)}</h1>
-            <svg width={width} height={height}>
-      <rect fill={background} width={width} height={height} rx={14} />
-      <Group top={height / 2 - margin.top} left={width / 2}>
-        {[...new Array(levels)].map((_, i) => (
-          <LineRadial
-            key={`web-${i}`}
-            data={webs}
-            angle={(d) => radialScale(d.angle) ?? 0}
-            radius={((i + 1) * radius) / levels}
-            fill="none"
-            stroke={silver}
-            strokeWidth={2}
-            strokeOpacity={0.8}
-            strokeLinecap="round"
-          />
-        ))}
-        {[...new Array(radarData.length)].map((_, i) => (
-          <Line key={`radar-line-${i}`} from={zeroPoint} to={points[i]} stroke={silver} />
-        ))}
-        <polygon
-          points={polygonPoints.pointString}
-          fill={orange}
-          fillOpacity={0.3}
-          stroke={orange}
-          strokeWidth={1}
-        />
-        {polygonPoints.points.map((point, i) => (
-          <circle key={`radar-point-${i}`} cx={point.x} cy={point.y} r={4} fill={pumpkin} />
-        ))}
-      </Group>
-    </svg>
-        </div>
-    ) : <p>loading</p>
+  const webs = genAngles(radarData.length);
+  const points = genPoints(radarData.length, radius);
+  const polygonPoints = genPolygonPoints(radarData, (d) => verticalScale(d) ?? 0, y);
+  const origin = new Point({ x: 0, y: 0 });
+  
+  return (
+    <div>
+      <svg width={width} height={height}>
+        <rect fill={background} width={width} height={height} rx={14} />
+        <Group top={height / 2 - margin.top} left={width / 2}>
+          {[...new Array(nLevels)].map((_, i) => (
+            <LineRadial
+              key={`web-${i}`}
+              data={webs}
+              angle={(d) => radialScale(d.angle) ?? 0}
+              radius={((i + 1) * radius) / nLevels}
+              fill="none"
+              stroke={silver}
+              strokeWidth={2}
+              strokeOpacity={0.8}
+              strokeLinecap="round"
+            />
+          ))}
+          {[...new Array(radarData.length)].map((_, i) => (
+            <Line key={`radar-line-${i}`} from={origin} to={points[i]} stroke={silver} />
+          ))}
+          <polygon points={polygonPoints.pointString} fill={peterRiver} fillOpacity={0.3} stroke={peterRiver} strokeWidth={1} />
+          {polygonPoints.points.map((point, i) => (
+            <circle key={`radar-point-${i}`} cx={point.x} cy={point.y} r={4} fill={belizeHole} />
+          ))}
+        </Group>
+      </svg>
+    </div>
+  )
 }
