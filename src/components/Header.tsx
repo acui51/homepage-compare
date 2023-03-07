9382dc1cf8c4a5defa3f6ebef631d4d2f9716022
acui51 <acui@stanford.edu>
2023-03-07 10:11:38 -0800

    Replace icons w antd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { poppins } from "@/pages/_app";
-import { AiFillCamera } from "react-icons/ai";
+import { CameraFilled } from "@ant-design/icons";
 
 const Header = () => {
   return (
@@ -7,19 +7,17 @@ const Header = () => {
       <div className="max-w-7xl w-full flex justify-between py-4 items-center">
         <div className="flex flex-row justify-between items-center">
           <div className="flex flex-row justify-between items-center mr-14">
-            <AiFillCamera style={{ color: '#5F6D7E', marginRight: 5 }} size={15} />
+            <CameraFilled
+              style={{ color: "#5F6D7E", marginRight: 5, fontSize: "15px" }}
+            />
             <div className={`font-semibold text-s text-[#5F6D7E] font-sans`}>
               Storytracker
             </div>
           </div>
-          <div className={`text-xs text-[#5F6D7E] font-sans`}>
-            Search
-          </div>
+          <div className={`text-xs text-[#5F6D7E] font-sans`}>Search</div>
         </div>
-        
-        <div className={`text-xs text-[#5F6D7E] font-sans`}>
-          Updated hourly
-        </div>
+
+        <div className={`text-xs text-[#5F6D7E] font-sans`}>Updated hourly</div>
       </div>
     </div>
   );
