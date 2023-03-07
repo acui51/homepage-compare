f1ffbad3c778e4c8497aeef6f11f981d1de94e1f
ting <tinglin0708@gmail.com>
2023-03-07 01:30:28 -0800

    updated header ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,23 @@
 import { poppins } from "@/pages/_app";
+import { AiFillCamera } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <div className="bg-[#1677ff] flex justify-center px-24 drop-shadow">
+    <div className="flex justify-center px-[110px]">
       <div className="max-w-7xl w-full flex justify-between py-4 items-center">
-        <div className={`font-bold text-2xl text-white ${poppins.className}`}>
-          Storytracker
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-row justify-between items-center mr-14">
+            <AiFillCamera style={{ color: '#5F6D7E', marginRight: 5 }} size={15} />
+            <div className={`font-semibold text-s text-[#5F6D7E] font-sans`}>
+              Storytracker
+            </div>
+          </div>
+          <div className={`text-xs text-[#5F6D7E] font-sans`}>
+            Search
+          </div>
         </div>
-        <div className={`text-white text-sm ${poppins.className}`}>
+        
+        <div className={`text-xs text-[#5F6D7E] font-sans`}>
           Updated hourly
         </div>
       </div>
