0033f223d2e06eb6bceb4f473fe92c66a8a87404
ting <74850954+tinglinn@users.noreply.github.com>
2023-03-08 23:55:12 -0800

    landing page font sizes (#14)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,18 +3,18 @@ import { CameraFilled } from "@ant-design/icons";
 
 const Header = () => {
   return (
-    <div className="flex justify-center md:px-24 px-4">
+    <div className="flex justify-center md:px-24 px-4 border-b border-[#5F6D7E]">
       <div className="max-w-7xl w-full flex justify-between py-4 items-center">
         <div className="flex flex-row justify-between items-center mr-5 4md:mr-14">
           <CameraFilled
-            style={{ color: "#5F6D7E", marginRight: 5, fontSize: "15px" }}
+            style={{ color: "#5F6D7E", marginRight: 5, fontSize: "16px" }}
           />
-          <div className={`font-semibold text-s text-[#5F6D7E] font-sans`}>
+          <div className={`font-semibold text-base text-[#5F6D7E] font-sans`}>
             Storytracker
           </div>
         </div>
 
-        <div className={`text-xs text-[#5F6D7E] font-sans`}>Updated hourly</div>
+        <div className={`text-sm text-[#5F6D7E] font-sans`}>Updated hourly</div>
       </div>
     </div>
   );
