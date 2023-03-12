c1a0d9f374bc2a90c591b6015d50f8ecfd86357b
Matthew Turk <mjturk15@gmail.com>
2023-03-11 17:11:30 -0800

    Okay, I think something like this might do

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -16,22 +16,28 @@ export default function Legend({ items, winningTerm }: LegendProps) {
   // two column layout with tailwind
   return (
     <div className="flex flex-col max-w-md" style={{marginTop: -80}}>
-      <div className="flex flex-row justify-between">
+
         <div className="flex flex-col">
-          {items.map((item, i) => (
-            <div key={i.toString()} className="flex flex-row">
-              <div
-                className="w-4 h-4 rounded-full"
-                style={{ backgroundColor: item.color.fill }}
-              />
-              <div className="ml-2"><p className='text-xs'>{item.label}</p></div>
+          <p>dofhusdofhofuid</p>
+          <div className="flex items-center flex-row">
+            <div className="w-4 h-4 mr-2 rounded-sm" style={{backgroundColor: 'white'}}></div>
+            <div className='max-width-prose'>
+              <p className='flex max-w-x text-xs'>“{winningTerm}” is the most common term found in the coverage from the selected date range.</p>
             </div>
-          ))}
-        </div>
-        <div className="flex flex-col max-w-xs">
-          <p className='flex max-w-x text-xs'>“{winningTerm}” is the most common term found in the coverage from the selected date range.</p>
-        </div>
+
+    
+          </div>
+          <div className="flex flex-row justify-around">
+        <div className="flex flex-row">
+          {items.map((item, i) => (
+        <div key={i.toString()} className="flex items-center">
+        <div className="w-4 h-4 mr-2 rounded-sm" style={{backgroundColor: item.color.fill}}></div>
+        <div>{item.label}</div>
       </div>
+          ))}
+          </div>
+          </div>
+        </div>
     </div>
   );
 }
