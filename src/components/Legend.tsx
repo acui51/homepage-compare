8afa438b8e1a18e4c4217794408a924aee1211a6
Matthew Turk <mjturk15@gmail.com>
2023-03-11 17:33:34 -0800

    Almost ready for pull request

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -16,28 +16,24 @@ export default function Legend({ items, winningTerm }: LegendProps) {
   // two column layout with tailwind
   return (
     <div className="flex flex-col max-w-md" style={{marginTop: -80}}>
-
-        <div className="flex flex-col">
-          <p>dofhusdofhofuid</p>
-          <div className="flex items-center flex-row">
-            <div className="w-4 h-4 mr-2 rounded-sm" style={{backgroundColor: 'white'}}></div>
-            <div className='max-width-prose'>
-              <p className='flex max-w-x text-xs'>“{winningTerm}” is the most common term found in the coverage from the selected date range.</p>
-            </div>
-
-    
-          </div>
-          <div className="flex flex-row justify-around">
-        <div className="flex flex-row">
-          {items.map((item, i) => (
-        <div key={i.toString()} className="flex items-center">
-        <div className="w-4 h-4 mr-2 rounded-sm" style={{backgroundColor: item.color.fill}}></div>
-        <div>{item.label}</div>
-      </div>
-          ))}
-          </div>
+      <div className="flex flex-col">
+        
+        <div className="flex flex-row justify-evenly">
+          <div className="flex flex-row">
+            {items.map((item, i) => (
+              <div key={i.toString()} className="flex items-center px-1">
+                <div className="w-4 h-4 mr-2 rounded-sm" style={{backgroundColor: item.color.fill}}></div>
+                <div>
+                  <p className='text-xs'>{item.label}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
+        <div className="flex flex-row justify-start px-2 py-2">
+          <p className='flex max-w-x text-sm text-left'>“{winningTerm}” is the most common term found in the coverage from the selected date range.</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/NewsPreview.tsx
+++ src/components/NewsPreview.tsx
@@ -64,7 +64,7 @@ export type NewsPreviewProps = {
     fullMulti: {
       [key: string]: RadarProminence[]
     }
-    colors: {
+    colors?: {
       [key: string]: PolygonColor
     }
 }
